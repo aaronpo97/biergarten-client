3a84f0cef20faf10c96b9b16266ffff9d1063e05
Aaron William Po <apo2@uwo.ca>
2022-06-21 12:31:21 -0400

    Scaffolded app with vite, eslint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import './index.css';
+
+const App: FC<{}> = () => (
+  <div className="flex items-center justify-center h-screen">
+    <div>
+      <h1 className="font-bold text-2xl">Hello world!</h1>
+    </div>
+  </div>
+);
+
+export default App;
